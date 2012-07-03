3da2165ebc3f64af72a9a97981defd43fac1a6fa
Yongzhi Wang <wongyouth@gmail.com>
2012-07-03 10:46:24 +0800

    init import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoga::Application.config.secret_token = 'de512ad95a53e67d08b2d287a534514f23a20c3d6afeeae80977f20bd14bbe5a6aadafa56b18d8ecc4e3853aefc8ba16ff5c65d7daec0e2a994ad3074acb008a'
